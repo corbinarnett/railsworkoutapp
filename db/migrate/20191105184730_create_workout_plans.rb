8c2fe8b8516a955ecfd4c88b675380050e7e2370
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-05 11:10:48 -0800

    add user_id to migration table

--- db/migrate/20191105184730_create_workout_plans.rb
+++ db/migrate/20191105184730_create_workout_plans.rb
@@ -1,6 +1,7 @@
 class CreateWorkoutPlans < ActiveRecord::Migration[6.0]
   def change
     create_table :workout_plans do |t|
+      t.integer :user_id
       t.string :title
       t.text :description
       t.boolean :active
