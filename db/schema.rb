a6f21200e8fef0d4a54254e5e213e8ab96ef7e2a
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-05 11:12:40 -0800

    run db:migrate, create schema

---
+++ db/schema.rb
@@ -0,0 +1,57 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_11_05_185844) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "workout_id", null: false
+    t.string "name"
+    t.integer "number_of_sets"
+    t.integer "weight"
+    t.integer "reps"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["workout_id"], name: "index_exercises_on_workout_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "workout_plans", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "description"
+    t.boolean "active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string "title"
+    t.datetime "date"
+    t.string "feeling"
+    t.boolean "complete"
+    t.integer "workout_plan_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["workout_plan_id"], name: "index_workouts_on_workout_plan_id"
+  end
+
+  add_foreign_key "exercises", "workouts"
+  add_foreign_key "workouts", "workout_plans"
+end
