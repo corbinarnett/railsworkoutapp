b90eb23bc6b0870a1ef65946c2aec91ab1cd2fe3
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-05 22:41:53 -0800

    add controller actions

--- app/controllers/workout_plans_controller.rb
+++ app/controllers/workout_plans_controller.rb
@@ -1,2 +1,38 @@
 class WorkoutPlansController < ApplicationController
+  
+  def index
+    @workout_plans = current_user.workout_plans
+  end
+
+  def show
+    @workout_plan = WorkoutPlan.find_by(params[:id])
+  end
+
+  def new
+    @workout_plan = WorkoutPlan.new
+  end
+
+  def create
+    byebug
+    @workout_plan = current_user.workout_plans.build(workout_plan_params)
+    if @workout_plan.save
+      redirect_to workout_plan_path(@workout_plan)
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private 
+
+  def workout_plan_params
+    params.require(:workout_plan).permit(:title, :description, :active)
+  end
 end
