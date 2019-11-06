7faba21b5f2628385dec582faf70c61a861ac765
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-05 22:06:55 -0800

    update create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.valid? 
+      @user.save
       #user has been saved we now want to remeber the user when they go to the next page
       session[:user_id] = @user.id
       redirect_to user_path(@user)
