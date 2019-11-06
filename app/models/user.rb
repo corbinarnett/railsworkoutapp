1a841ab0392610dd9cbcc2d47e033ad0797d3393
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-05 21:17:41 -0800

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   has_many :workouts, through: :workout_plans
   #bycrpt gem macro
   has_secure_password
+
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 end
