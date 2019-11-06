6862c0dab0c75baaff8d7bc78151a578437586f6
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-05 23:55:15 -0800

    add category to exercise

---
+++ db/migrate/20191106075340_add_category_to_exercise.rb
@@ -0,0 +1,5 @@
+class AddCategoryToExercise < ActiveRecord::Migration[6.0]
+  def change
+    add_column :exercises, :category, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_185844) do
+ActiveRecord::Schema.define(version: 2019_11_06_075340) do
 
   create_table "exercises", force: :cascade do |t|
     t.integer "user_id"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_05_185844) do
     t.integer "reps"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
     t.index ["workout_id"], name: "index_exercises_on_workout_id"
   end
 
